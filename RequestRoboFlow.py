273e1f5f3355bd1615e15cc4fb5e2ae64924136a
Leo Pierangelli <leonardo.pierangelli@gmail.com>
2025-05-05 21:27:14 -0300

    a

--- RequestRoboFlow.py
+++ RequestRoboFlow.py
@@ -1,15 +1,15 @@
 from roboflow import Roboflow
 import json
-
+imagem = 'gaveta_2.jpg'
 rf = Roboflow(api_key="mB8Ak44KAuneBt65f5Im")
 project = rf.workspace().project("my-first-project-kl4s4")
 model = project.version(4).model
 
 # Visualizar sua predição
-model.predict("imagem 3.webp", confidence=40, overlap=30).save("predicao1.jpg")
+model.predict(imagem, confidence=40, overlap=30).save("predicao1.jpg")
 
 # Dicionário retornado:
-resultado = model.predict("imagem 3.webp", confidence=40, overlap=30).json()
+resultado = model.predict(imagem, confidence=40, overlap=30).json()
 
 # Convertendo para string JSON formatada
 resultado_json = json.dumps(resultado, indent=2)
